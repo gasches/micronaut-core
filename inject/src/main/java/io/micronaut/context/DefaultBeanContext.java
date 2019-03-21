63737c91a3edc6efedca0cc6d8f7fbaf06aaeeb6
graemerocher <graeme.rocher@gmail.com>
2019-03-21 12:43:05 +0100

    Progress implementing javax.validation.constraints

--- inject/src/main/java/io/micronaut/context/DefaultBeanContext.java
+++ inject/src/main/java/io/micronaut/context/DefaultBeanContext.java
@@ -2193,6 +2193,7 @@ public class DefaultBeanContext implements BeanContext {
             for (AnnotationValue<Indexed> index : indexes) {
                 final Class indexedType = index.getValue(Class.class).orElse(null);
                 if (indexedType != null) {
+                    indexedTypes.add(indexedType);
                     final Collection<BeanDefinitionReference> indexed = resolveTypeIndex(indexedType);
                     indexed.add(beanDefinitionReference);
                 }
